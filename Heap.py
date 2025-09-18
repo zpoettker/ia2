0ab98a5f2a8c5e230ddb4ec3929b241852b0c1eb
zpoettker <poettkerzach@gmail.com>
2025-09-18 17:38:20 -0500

    Update Heap.py

--- Heap.py
+++ Heap.py
@@ -1,20 +1,22 @@
+
 class Heap:
+    #Constructor
     def __init__(self, arr=None):
-       #Constructor
         if arr is None:
             self.heap = []
         else:
             self.heap = arr[:]
             self.build_heap()
 
+    #Destructor
     def __del__(self):
-        #Destructor
         del self.heap
 
     def find_left(self, i):
         left = 2 * i + 1
         if left < len(self.heap):
             return left
+            
         return None
 
     def find_right(self, i):
@@ -27,13 +29,13 @@ class Heap:
     def find_parent(self, i):
         if i == 0:
             return None
-            
         return (i - 1) // 2
 
     def get_value(self, i):
         if 0 <= i < len(self.heap):
+            
             return self.heap[i]
-    
+            
         return None
 
     def heap(self, i):
@@ -52,7 +54,33 @@ class Heap:
             self.heapify(largest)
 
     def build_heap(self):
-        
         n = len(self.heap)
         for i in range((n // 2) - 1, -1, -1):
             self.heapify(i)
+
+    def extract_max(self):
+        if len(self.heap) == 0:
+            return None
+            
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.heapify(0)
+        
+        return root
+
+    def insert(self, value):
+        self.heap.append(value)
+        i = len(self.heap) - 1
+        while i > 0 and self.heap[self.find_parent(i)] < self.heap[i]:
+            parent = self.find_parent(i)
+            self.heap[i], self.heap[parent] = self.heap[parent], self.heap[i]
+            i = parent
+
+    def heap_sort(self):
+        result = []
+        while len(self.heap) > 0:
+            result.insert(0, self.extract_max())
+            
+        return result
